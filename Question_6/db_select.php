1c112d68cfd341993f61e787f10b3c8e32940f78
N8MARERahul <pal521843@gmail.com>
2022-12-29 22:54:06 +0530

    modified

--- Question_6/db_select.php
+++ Question_6/db_select.php
@@ -2,28 +2,24 @@
 
     require "E:\applications/xampp\htdocs\Assignments\Question_2\db_con.php";
 
-    $querry = "SELECT * from student";
+    $querry = "SELECT * from login";
 
     $result = mysqli_query($connection, $querry);
 ?>
 <table border ="2" cellspacing = "0" cellpadding="10">
     <tr>
-        <th>Name</th>
-        <th>Roll</th>
-        <th>City</th>
-        <th>Email</th>
-        <th>D_O_B</th>
+        <th>ID</th>
+        <th>User Name</th>
+        <th>Password</th>
     </tr>
 <?php
     if(mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_array($result)) {
 ?>  
     <tr>
-        <td><?php echo $row["name"]; ?></td>
-        <td><?php echo $row["roll"]; ?></td>
-        <td><?php echo $row["city"]; ?></td>
-        <td><?php echo $row["email"]; ?></td>
-        <td><?php echo $row["d_o_b"]; ?></td>
+        <td><?php echo $row["id"]; ?></td>
+        <td><?php echo $row["username"]; ?></td>
+        <td><?php echo $row["password"]; ?></td>
     </tr>     
 <?php     
         } 
